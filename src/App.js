fcf69a57c7e753ca89b9cd623d6998fb376b780d
vangarde03 <alexcobra7743@gmail.com>
2024-07-21 18:10:57 -0400

    Week 7 Completed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,36 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavbarComp from './component/navigation/NavbarComp';
+import * as api from './services/api';
+import React, { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <NavbarComp/>
+      {!authenticated ? (
+        <div>
+          <label>Username: </label>
+          <br />
+          <input
+            type="text"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <br />
+          <label>Password: </label>
+          <br />
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <button onClick={createUser}>Create User</button>
+          <button onClick={authUser}>Login</button>
+        </div>
+      ) : (
+        <div className="App">
+          <NavbarComp />
+        </div>
+      )}
     </div>
   );
 }
